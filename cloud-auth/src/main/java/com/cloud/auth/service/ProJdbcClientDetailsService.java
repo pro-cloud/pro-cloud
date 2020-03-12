bf088fc32517fdda17bd25b09fc40fe7d3609df7
Crazy <2929783435qq.com>
2020-03-12 18:09:55 +0800

    perf: swagger 升级

--- cloud-auth/src/main/java/com/cloud/auth/service/ProJdbcClientDetailsService.java
+++ cloud-auth/src/main/java/com/cloud/auth/service/ProJdbcClientDetailsService.java
@@ -1,5 +1,10 @@
 package com.cloud.auth.service;
 
+import com.cloud.common.cache.annotation.Cache;
+import com.cloud.common.cache.annotation.CachePut;
+import com.cloud.common.cache.constants.CacheScope;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +20,11 @@ public class ProJdbcClientDetailsService extends JdbcClientDetailsService {
 
     public ProJdbcClientDetailsService(DataSource dataSource) {
         super(dataSource);
+    }
+
+    @Override
+    @Cache( scope = CacheScope.AUTH_CLIENT,  key = "#clientId" , expire = 3600)
+    public ClientDetails loadClientByClientId(String clientId) throws InvalidClientException {
+        return super.loadClientByClientId(clientId);
     }
 }

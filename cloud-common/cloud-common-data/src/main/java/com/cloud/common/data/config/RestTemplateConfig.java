085630c27afd0d59db4e4bd68468c4e2f7d92fb8
pro-cloud <jxaijm94@163.com>
2020-04-01 22:27:42 +0800

    perf: 优化RestTemplate

--- cloud-common/cloud-common-data/src/main/java/com/cloud/common/data/config/RestTemplateConfig.java
+++ cloud-common/cloud-common-data/src/main/java/com/cloud/common/data/config/RestTemplateConfig.java
@@ -10,7 +10,6 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 
 /**
  * 配置 类
@@ -20,30 +19,15 @@ import java.io.IOException;
 @Configuration
 public class RestTemplateConfig {
 
+
+    /**
+     * RestTemplate的声明
+     * @param builder
+     * @return
+     */
     @Bean
     @LoadBalanced
-    @Primary
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new DefaultResponseErrorHandler(){
-            @Override
-            public void handleError(ClientHttpResponse response) throws IOException {
-                if(response.getRawStatusCode()!= 400 && response.getRawStatusCode()!= 401){
-                    super.handleError(response);
-                }
-            }
-
-        });
-
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
-
-
-
-//    @Bean
-//    // 添加负载均衡支持，很简单，只需要在RestTemplate上添加@LoadBalanced注解，那么RestTemplate即具有负载均衡的功能,如果不加@LoadBalanced注解的话，会报java.net.UnknownHostException:springboot-h2异常，此时无法通过注册到Nacos Server上的服务名来调用服务，因为RestTemplate是无法从服务名映射到ip:port的，映射的功能是由LoadBalancerClient来实现的。
-//    @LoadBalanced
-//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        return builder.build();
-//    }
 }

b191f53cf4f821ad3eadf13493115e7b9b271c89
pro-cloud <jxaijm94@163.com>
2020-06-04 22:34:05 +0800

    perf: 校验异常处理

--- cloud-common/cloud-common-data/src/main/java/com/cloud/common/data/exception/GlobalExceptionHandler.java
+++ cloud-common/cloud-common-data/src/main/java/com/cloud/common/data/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.cloud.common.data.base.Result;
 import com.cloud.common.data.enums.ResultEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,12 +29,28 @@ public class GlobalExceptionHandler {
         return Result.error(e.getCode(), e.getMessage());
     }
 
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    /**
+     * 所有验证框架异常捕获处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
     @ResponseStatus(HttpStatus.OK)
-    public Result handleValidException(MethodArgumentNotValidException e){
+    public Result handleValidException(Exception e){
+        BindingResult bindResult = null;
+        if (e instanceof BindException) {
+            bindResult = ((BindException) e).getBindingResult();
+        } else if (e instanceof MethodArgumentNotValidException) {
+            bindResult = ((MethodArgumentNotValidException) e).getBindingResult();
+        }
+        String msg;
+        if (bindResult != null && bindResult.hasErrors()) {
+            msg = bindResult.getAllErrors().get(0).getDefaultMessage();
+        }else {
+            msg = "系统繁忙，请稍后重试...";
+        }
         log.error("参数校验异常，msg:{}",  e.getMessage(), e);
-        return Result.error(ResultEnum.CRUD_VALID_NOT);
+        return Result.error(ResultEnum.CRUD_VALID_NOT.getCode(), msg);
     }
 
     @ExceptionHandler(Exception.class)
